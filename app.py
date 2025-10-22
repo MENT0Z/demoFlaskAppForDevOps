e9b9b0db61145cb5df2dcbea02caa67269a6c38a
Madan <mrmrupadhyau@gmail.com>
2025-10-22 20:32:21 +0530

    changed the app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask deployed on AWS EKS via Jenkins!"
+    return "Hello from Flask deployed on AWS EKS via Jenkins! By Madan Raj Upadhyay(RA2211003012077)"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
